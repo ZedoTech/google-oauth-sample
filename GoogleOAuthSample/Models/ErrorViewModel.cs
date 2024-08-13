db1c039acdd1e52d41a06b96d012585fbe30b7d1
Nicky Liu <nicky@zedotech.com>
2024-08-13 21:49:14 +0800

    加入專案檔案。

---
+++ GoogleOAuthSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleOAuthSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
